a194769f66b10469fd479796f7f6ca9a510f15e7
arya2005getit <aryadilliwale1006@gmail.com>
2025-09-04 23:25:13 +0530

    Create Practical3.cpp

---
+++ Practical3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class demo {
+	int x,y,z;
+public :
+	void getdata(int a, int b, int c){
+		x=a;
+		y=b;
+		z=c;
+	}
+	void display();
+	void operator-();
+};
+						
+void demo::display(){
+	cout << "x: " << x << "\ny: " << y << "\nz: " << z << endl ;
+}
+
+void demo::operator-(){
+	x=-x;
+	y=-y;
+	z=-z;
+}
+
+int main(){
+	demo obj1 ;
+	obj1.getdata(10,20,30);
+	obj1.display();
+	-obj1;
+	obj1.display();
+	return 0;
+}
